d2badfaea18f0efbffaec1ed2e7e99e9a0e12cbd
MOTER HAMEEDI <edm2n.ios@gmail.com>
2026-09-07 19:05:09 +0300

    Update api.js

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API = `${process.env.REACT_APP_BACKEND_URL}/api`;
+export const API = `${import.meta.env.VITE_BACKEND_URL || ""}/api`;
 
 const api = axios.create({ baseURL: API });
 
