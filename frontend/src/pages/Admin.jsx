0dafff69e38f7cfc089de81c1fa62fde7f9fb18d
MOTER HAMEEDI <edm2n.ios@gmail.com>
2026-09-08 15:15:07 +0300

    Update Admin.jsx

--- frontend/src/pages/Admin.jsx
+++ frontend/src/pages/Admin.jsx
@@ -32,7 +32,6 @@ export default function AdminDashboard({ activeTab = 'students', setActiveTab })
   });
 
   // جلب الطلاب والرسائل والإعدادات من الباك إند
-  // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(() => {
     if (activeTab === 'students') {
       fetchStudents();
@@ -41,6 +40,7 @@ export default function AdminDashboard({ activeTab = 'students', setActiveTab })
     } else if (activeTab === 'settings') {
       fetchSettings();
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [activeTab]);
 
   // تحويل من صيغة الباك إند (snake_case) إلى صيغة الواجهة (camelCase)
